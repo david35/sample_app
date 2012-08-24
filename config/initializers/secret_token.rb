10acdb0446aa98051e8ff27f227e83cc8319e7b3
Bo He <bo.he@funtongames.com>
2012-08-24 15:11:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '169e395c7d19c6e11e5ad5d020ab859d239f36b43d95c94c367b89ba7d13331b989bc16d6aba62ffe911f7cb0fb0ec9025fc3971001da0ecdd473163c2461e45'
